e611fabead13c19d4d4c259a31881b832e222cb4
vuk5 <102692209+vuk5@users.noreply.github.com>
2022-06-01 21:23:10 +0530

    Time: 3 ms (59.31%), Space: 5.9 MB (80.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,17 @@
 class Solution {
     
+    
 public:
     int fib(int n) {
-        if(n==0 or n==1)return n;
-        return fib(n-1)+fib(n-2);
+        if(n<=1)return n;
+       int prev2=0,prev1=1;
+        for(int i=2;i<=n;i++){
+           int cur=prev2+prev1;
+
+            prev2=prev1;
+                        prev1=cur;
+            
+        } 
+        return prev1;
     }
 };
