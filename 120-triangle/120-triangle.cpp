fbd9318efadfb0ea2917cd5602cef8c66674ef24
vuk5 <102692209+vuk5@users.noreply.github.com>
2022-06-13 21:20:38 +0530

    Time: 4 ms (89.10%), Space: 9.1 MB (9.59%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,15 +1,21 @@
 class Solution {
-   int f(int i,int j, vector<vector<int>>& triangle,int n,vector<vector<int>> &ans){
-        if(i==n)return triangle[i][j];
-        if(ans[i][j]!=-1)return ans[i][j];
-        return ans[i][j]= min(f(i+1,j,triangle,n,ans)+triangle[i][j],f(i+1,j+1,triangle,n,ans)+triangle[i][j]);
-        
-        
-    }
+   
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>> ans(n,vector<int>(n,-1));
-        return f(0,0,triangle,n-1,ans);
-    }
+        
+       for(int j=0;j<n;j++)
+           ans[n-1][j]=triangle[n-1][j];
+        
+       for(int i=n-2;i>=0;i--){
+           for(int j=i;j>=0;j--){
+               int dow=triangle[i][j]+ans[i+1][j];
+               int down=triangle[i][j]+ans[i+1][j+1];
+               ans[i][j]=min(dow,down);
+           }
+       }
+        return ans[0][0];
+        
+        }
 };
