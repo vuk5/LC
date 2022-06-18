d5d4095a00fec1ca11f24c4bca943a50bcabd655
vuk5 <102692209+vuk5@users.noreply.github.com>
2022-06-18 13:33:06 +0530

    Time: 17 ms (73.77%), Space: 9.9 MB (63.22%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -5,33 +5,34 @@ public:
       int n = matrix.size();
     int m = matrix[0].size();
     
-    vector<vector<int>> ans(n,vector<int>(m,0));
+   vector<int> prev(m,0),cur(m,0);
     
     // Initializing first row - base condition
     for(int j=0; j<m; j++){
-        ans[0][j] = matrix[0][j];
+       prev[j] = matrix[0][j];
     }
     
     for(int i=1; i<n; i++){
         for(int j=0;j<m;j++){
             
-            int up = matrix[i][j] + ans[i-1][j];
+            int up = matrix[i][j] + prev[j];
             
             int leftDiagonal= matrix[i][j];
-            if(j-1>=0) leftDiagonal += ans[i-1][j-1];
+            if(j-1>=0) leftDiagonal += prev[j-1];
             else leftDiagonal += 1e9;
             
             int rightDiagonal = matrix[i][j];
-            if(j+1<m) rightDiagonal += ans[i-1][j+1];
+            if(j+1<m) rightDiagonal += prev[j+1];
             else rightDiagonal += 1e9;
             
-            ans[i][j] = min(up, min(leftDiagonal,rightDiagonal));
+            cur[j] = min(up, min(leftDiagonal,rightDiagonal));
             
         }
+        prev=cur;
     }
         int mini=INT_MAX;
         for(int j=0;j<m;j++){
-            mini=min(mini,ans[n-1][j]);
+            mini=min(mini,prev[j]);
         }
         return mini;
     }
