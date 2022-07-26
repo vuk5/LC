503e8bf64cd30d8b663ec2907a060b39e0b76931
vuk5 <102692209+vuk5@users.noreply.github.com>
2022-07-26 19:43:57 +0530

    Time: 62 ms (38.48%), Space: 71.6 MB (11.72%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -8,29 +8,32 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//ADD TWO NUMBERS IN LINKED LIST
+//TC=O(MAX(M,N)),SC=O(MAX(M,N))
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy=new ListNode();
-        ListNode* temp=dummy;
-        int carry=0;
-        while(l1!=NULL or l2!=NULL or carry!=0){
-            int sum=0;
-            if(l1!=NULL){
-                sum+=l1->val;
-                l1=l1->next;
+        ListNode *dummy = new ListNode(); //stores address of new lists head
+        ListNode *temp = dummy;  //temp pointer to traverse the new list
+        int carry = 0;
+        while( (l1 != NULL || l2 != NULL) || carry) {
+            int sum = 0; 
+            if(l1 != NULL) {
+                sum += l1->val; 
+                l1 = l1 -> next; 
             }
-            if(l2!=NULL){
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            sum+=carry;
-           ListNode* node= new ListNode(sum%10);
-            carry=sum/10;
-            temp->next=node;
-            temp=temp->next;
             
+            if(l2 != NULL) {
+                sum += l2 -> val; 
+                l2 = l2 -> next; 
+            }
+            
+            sum += carry; 
+            carry = sum / 10; 
+            ListNode *node = new ListNode(sum % 10); 
+            temp -> next = node; 
+            temp = temp -> next;  //move through the newly created nodes
         }
-        return dummy->next;
+        return dummy -> next;     //return head of new linked list
     }
 };
