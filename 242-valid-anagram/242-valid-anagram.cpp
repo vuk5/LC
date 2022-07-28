7ecef218e0a536990b1931820eb560f3965f2f88
vuk5 <102692209+vuk5@users.noreply.github.com>
2022-07-28 16:27:36 +0530

    Time: 10 ms (74.53%), Space: 7.5 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        vector<int> freq1(256,0),freq2(256,0);
+        for(int i=0;i<s.size();i++){
+            freq1[s[i]-'0']++;
+        }
+        for(int i=0;i<t.size();i++){
+            freq2[t[i]-'0']++;
+        }
+        for(int i=0;i<freq1.size();i++)
+        {
+            if(freq1[i]!=freq2[i])return false;
+        }
+        return true;
     }
 };
