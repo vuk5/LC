b0a61082ba67913e6dc28dce2425ed6e5d283974
Vignesh <102692209+vuk5@users.noreply.github.com>
2022-08-13 21:44:22 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (80.03%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        int res=0,cl=0;
+       int ans=0;
+        int cl=0;
         for(auto it:s){
             if(it=='L')cl++;
             else cl--;
-            if(cl==0)res++;
+            if(cl==0)ans++;
         }
-        return res;
+        return ans;
     }
 };
