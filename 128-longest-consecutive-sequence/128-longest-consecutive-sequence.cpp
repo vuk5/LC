a233764cc4abbface53cd546fc4cbb44f67d6687
Vignesh <102692209+vuk5@users.noreply.github.com>
2022-08-19 18:50:10 +0530

    Time: 1207 ms (9.58%), Space: 45.6 MB (73.07%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,32 +1,19 @@
-
-//Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
-
-//TC=O(N),SC=O(1)
-
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if(nums.size()==0)return 0;
-        if(nums.size()==1)return 1;
-        unordered_set<int> s;
-        for(auto it:nums){
-            s.insert(it);      //initially insert the numbers in array to set
-        }
-        int maxi=0;
-        //if nums[i]-1 exists for nums[i] continue
-        
+       unordered_set<int> s(nums.begin(),nums.end());
+        int maxi=INT_MIN;
+        int count;
         for(int i=0;i<nums.size();i++){
-            if(s.find(nums[i]-1)==s.end()){       //if there is no previous num in set
-               int cur=nums[i];
-                int count=1;
-                while(s.find(cur+1)!=s.end()){      //increase count till the next is not there in set
-                    count++;
-                    cur++;
-                    
-                }
-                maxi=max(maxi,count);                //return maximum  of all counts
+            if(s.count(nums[i]-1)!=0)continue;             //we want to start from element which has no previous
+            count=0;
+            while(s.count(nums[i])!=0){
+                count++;
+                nums[i]+=1;
             }
+            maxi=max(maxi,count);
         }
-            return maxi;
+        return maxi;
     }
 };
