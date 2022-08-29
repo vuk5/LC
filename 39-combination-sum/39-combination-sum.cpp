5edb38e4ee4f8722e1872bbff34c8047c7bef120
Vignesh <102692209+vuk5@users.noreply.github.com>
2022-08-29 18:51:13 +0530

    Time: 197 ms (15.43%), Space: 69.1 MB (13.59%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,28 +1,27 @@
 class Solution {
-    
-void recurse(int ind,vector<int> nums,vector<vector<int>>&ans,vector<int> &ds,int target){
-    if(ind==nums.size()){
-        if(target==0){
+public:
+    void f(int ind,vector<int>nums,vector<vector<int>>&ans,vector<int>&ds,int target){
+        if(ind==nums.size()){
+            if(target==0){
             ans.push_back(ds);
-            
         }
-        return;
+            return;
+        }
+       
+        if(nums[ind]<=target)
+        {   ds.push_back(nums[ind]);
+            f(ind,nums,ans,ds,target-nums[ind]);
+         ds.pop_back();
+        }
+         f(ind+1,nums,ans,ds,target);
+        
+        
+        
     }
-    if(nums[ind]<=target){
-        ds.push_back(nums[ind]);
-        recurse(ind,nums,ans,ds,target-nums[ind]);
-        ds.pop_back();
-    }
-    recurse(ind+1,nums,ans,ds,target);
-}
-
-  public:
-    vector < vector < int >> combinationSum(vector < int > & nums, int target) {
-      vector<vector<int>> ans;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
         vector<int> ds;
-        recurse(0,nums,ans,ds,target);
+        f(0,nums,ans,ds,target);
         return ans;
     }
-
-    
 };
