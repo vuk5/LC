05525ea77e4e06bb0a25a2d93743dcc557d468a6
Vignesh <102692209+vuk5@users.noreply.github.com>
2022-08-30 18:56:48 +0530

    Time: 2 ms (67.40%), Space: 7.2 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -8,6 +8,8 @@ public:
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i=0;i<n;i++)reverse(matrix[i].begin(),matrix[i].end());
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
